fc35ff41041b9cc659ef3d7e49df64352d4dd75d
Sylvia Barcena <barcena.sylvia8@gmail.com>
2023-08-29 14:43:00 -0700

    init

--- more/Menu/menu.js
+++
@@ -1,7 +0,0 @@
-if (window.matchMedia("(min-width: 400px)").matches) {
-    // make all the navigation visible
-    // hide the hamburger icon
-  } else {
-    // show the hamburger icon
-    // hide the navigation until someone presses the hamburger
-  }
